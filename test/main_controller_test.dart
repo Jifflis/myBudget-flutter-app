746d8059caa1f428eda2ed53b16e81d23d539acd
jifflisLIG <jeffrey_abaniel@ligph.com>
2021-04-12 15:35:25 +0800

    Add unit test in maincontroller set bottom index

--- test/main_controller_test.dart
+++ test/main_controller_test.dart
@@ -1,3 +1,11 @@
-void main(){
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mybudget/controller/main_controller.dart';
 
+void main() {
+  test('set/get bottom index', () {
+    MainController controller = MainController();
+    int testValue = 1;
+    controller.selectedBottomIndex.value = testValue;
+    expect(controller.selectedBottomIndex.value, testValue);
+  });
 }
