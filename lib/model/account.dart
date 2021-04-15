1357d35348a402ef0685233fc1c0d368f1755a02
jifflisLIG <jeffrey_abaniel@ligph.com>
2021-04-15 20:23:27 +0800

    fix review

---
+++ lib/model/account.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class BudgetModelSample {
+  final String title;
+  final double budget;
+  final double expense;
+
+  BudgetModelSample({
+    @required this.title,
+    @required this.budget,
+    @required this.expense,
+  });
+}
