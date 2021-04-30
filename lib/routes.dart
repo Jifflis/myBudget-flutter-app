9bca7ab8b308c565b80a2a256ac9108dfd26cfd1
japhethlowgurl <paypa.japheth@gmail.com>
2021-05-01 05:27:19 +0800

    Added View budget ui

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mybudget/screen/add_budget_screen.dart';
 import 'package:mybudget/screen/introduction_screen.dart';
+import 'package:mybudget/screen/view_budget_screen.dart';
 
 import 'screen/home_screen.dart';
 import 'screen/main_screen.dart';
@@ -21,6 +22,7 @@ class Routes {
   static const String SCREEN_SETTINGS = '/settings_screen';
   static const String SCREEN_TRANSACTIONS = '/transaction_screen';
   static const String SCREEN_ADD_BUDGET = '/add_budget_screen';
+  static const String SCREEN_VIEW_BUDGET = '/view_budget_screen';
 
   //navigator key ------------------------------------------------------------
   /// set navigators key.
@@ -85,6 +87,10 @@ class Routes {
 
       case SCREEN_ADD_BUDGET:
         screen = AddBudgetScreen();
+        break;
+
+      case SCREEN_VIEW_BUDGET:
+        screen = ViewBudgetScreen();
         break;
     }
 
