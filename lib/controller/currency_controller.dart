cb35a4b0bb507a83261bc67404734264e71cc19f
japhethlowgurl <paypa.japheth@gmail.com>
2021-05-06 16:12:32 +0800

    Fixed currency controller

--- lib/controller/currency_controller.dart
+++ lib/controller/currency_controller.dart
@@ -2,15 +2,10 @@ import 'dart:collection';
 
 import 'package:mybudget/controller/base_controller.dart';
 
-import '../enum/status.dart';
 import '../model/currency.dart';
-import '../model/settings.dart';
-import '../model/user.dart';
 import '../repository/currency_repository.dart';
 import '../repository/settings_repository.dart';
 import '../repository/user_repository.dart';
-import '../util/date_util.dart';
-import '../util/random_util.dart';
 
 class CurrencyController extends BaseController {
   final CurrencyRepository _currencyRepository = CurrencyRepository.instance;
@@ -43,43 +38,18 @@ class CurrencyController extends BaseController {
     update();
   }
 
+  /// Get settings repository
+  ///
+  SettingsRepository get settingsRepository => _settingsRepository;
+
+  /// Get user repository
+  ///
+  UserRepository get userRepository => _userRepository;
+
   /// Initialize [_currencyList] data
   ///
   void _initCurrencies() {
     _currencyList = _currencyRepository.currencyList;
     _selectedCurrency = _currencyList[0];
-  }
-
-  /// Save data into local database
-  /// It will create a default user
-  ///
-  Future<Status> save() async {
-    status = Status.LOADING;
-    final User user = User.factory();
-    await _saveUser(user);
-    await _saveSettings(user);
-    status = Status.COMPLETED;
-    return status;
-  }
-
-  /// Save Settings into local database
-  ///
-  Future<void> _saveSettings(User user) async {
-    final Settings settings = Settings(
-      settingsId: randomID(),
-      firstInstall: false,
-      refreshDate: getLastDateOfMonth(),
-      createdAt: DateTime.now(),
-      updatedAt: DateTime.now(),
-      currency: _selectedCurrency,
-      user: user,
-    );
-    await _settingsRepository.upsert(settings);
-  }
-
-  /// Save user into local database
-  ///
-  Future<void> _saveUser(User user) async {
-    await _userRepository.upsert(user);
   }
 }
--- lib/controller/introduction_controller.dart
+++ lib/controller/introduction_controller.dart
@@ -1,3 +1,41 @@
 import 'package:mybudget/controller/currency_controller.dart';
+import '../enum/status.dart';
+import '../model/settings.dart';
+import '../model/user.dart';
+import '../util/date_util.dart';
+import '../util/random_util.dart';
 
-class IntroductionController extends CurrencyController {}
+class IntroductionController extends CurrencyController {
+  /// Save data into local database
+  /// It will create a default user
+  ///
+  Future<Status> save() async {
+    status = Status.LOADING;
+    final User user = User.factory();
+    await _saveUser(user);
+    await _saveSettings(user);
+    status = Status.COMPLETED;
+    return status;
+  }
+
+  /// Save Settings into local database
+  ///
+  Future<void> _saveSettings(User user) async {
+    final Settings settings = Settings(
+      settingsId: randomID(),
+      firstInstall: false,
+      refreshDate: getLastDateOfMonth(),
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+      currency: selectedCurrency,
+      user: user,
+    );
+    await settingsRepository.upsert(settings);
+  }
+
+  /// Save user into local database
+  ///
+  Future<void> _saveUser(User user) async {
+    await userRepository.upsert(user);
+  }
+}
