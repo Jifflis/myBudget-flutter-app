3e9fc04685881663091e48275bb8795e8bfd8ae5
japhethlowgurl <paypa.japheth@gmail.com>
2021-05-06 09:51:59 +0800

    Fix imports

--- lib/controller/change_currency_controller.dart
+++ lib/controller/change_currency_controller.dart
@@ -1,4 +1,3 @@
-import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:mybudget/controller/introduction_controller.dart';
 
 class ChangeCurrencyController extends IntroductionController {
