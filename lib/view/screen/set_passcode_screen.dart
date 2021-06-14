d383649b0ff1f3c3ef991220da8533eb99cd0e35
unknown <paypa.japheth@gmail.com>
2021-06-14 10:16:36 +0800

    Update imports

--- lib/view/screen/set_passcode_screen.dart
+++ lib/view/screen/set_passcode_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_pin_code_fields/flutter_pin_code_fields.dart';
